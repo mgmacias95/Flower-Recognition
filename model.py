396815de007736a42265c1ec41783e1766f067a9
mgmacias95 <mgmacias95@correo.ugr.es>
2017-02-01 20:13:45 +0100

    separados en dos ficheros distintos

---
+++ model.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+
+from flower import *
+
+# train a model
+def train_model(model, label, data, mask=training_mask,
+                lay=cv2.ml.ROW_SAMPLE):
+    model.train(samples=data[mask], layout=lay,
+                responses=label[mask])
+
+
+def predict_model(model, label, data, mask):
+    return model.predict(samples=data[mask], results = label[mask])
+
+def error(labels, results):
+    return ((labels - results)**2).mean(axis=None)
+
+# Declare SVM model
+svm_model = cv2.ml.SVM_create()
+svm_model.setGamma(0.1)
+svm_model.setC(3.01)
+svm_model.setNu(0.68)
+svm_model.setKernel(cv2.ml.SVM_RBF)
+svm_model.setType(cv2.ml.SVM_NU_SVC)
+
+# train the svm model
+train_model(model=svm_model, label=df_labels, data=df_data_array)
+results = predict_model(svm_model, df_labels, df_data_array, mask=training_mask)
+train_error = error (df_labels[training_mask], results[1])
+print("Error en train = ", train_error)
+# test the svm model
+test_results = predict_model(svm_model, df_labels, df_data_array, mask=test_mask)
+test_error = error (df_labels[test_mask], test_results[1])
+print("Error en test = ", test_error)
+
+

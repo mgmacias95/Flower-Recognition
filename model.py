d56e443ab202e12ec823da8f652599748d4f12b6
mgmacias95 <mgmacias95@correo.ugr.es>
2017-02-04 15:49:14 +0100

    added random forest :)

--- model.py
+++ model.py
@@ -34,4 +34,15 @@ test_results = predict_model(svm_model, df_labels, df_data_array, mask=test_mask
 test_error = error (df_labels[test_mask], test_results[1])
 print("Error en test = ", test_error)
 
+# Declare Random Forest Model
+rt_model = cv2.ml.RTrees_create()
 
+# train the rt model
+train_model(model=rt_model, label=df_labels, data=df_data_array)
+results_rt = predict_model(model=rt_model, label=df_labels, data=df_data_array, mask=training_mask)
+train_error_rt = error(df_labels[training_mask], results_rt[1])
+print("Error en train de Random Forest = ", train_error_rt)
+
+test_results_rt = predict_model(model=rt_model, label=df_labels, data=df_data_array, mask=test_mask)
+test_error_rt = error(df_labels[test_mask], test_results_rt[1])
+print("Error en test de Random Forest = ", test_error_rt)

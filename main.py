c5dfe6a865eed6108bec05672b81f9a2c044eeda
BraulioV <brauliovarlop@correo.ugr.es>
2017-02-05 18:56:15 +0100

    BOW finished

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 import sys
-# import numpy as np
+import numpy as np
 import flower as fl
 import cluster_bow as cb
 
@@ -21,10 +21,7 @@ if __name__ == '__main__':
     images = [cv2.imread('Dataset/image_' + '%0*d' % (4, i) + '.jpg',
                          flags=cv2.IMREAD_COLOR) for i in range(1, 50)]
 
-    # num_photos_per_class = 80
-
     # Create geometric vocabulary of the images and then, we do K-Means
     # clustering to create the Bag Of Words and get the
     # labels and histograms of every class
-
     BOW = fl.create_bag_of_words(images, sys.argv[1].upper())
